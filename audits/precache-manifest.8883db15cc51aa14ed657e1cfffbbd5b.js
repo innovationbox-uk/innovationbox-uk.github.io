34797a36210a6eab3ce9958d3d04a5fa8d0b1072
werckerbot <pleasemailus@wercker.com>
2020-08-01 09:50:53 +0000

    deploy from davidtron

---
+++ audits/precache-manifest.8883db15cc51aa14ed657e1cfffbbd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d886b1578550dfaef6",
+    "url": "/audits/static/css/main.7940d89b.chunk.css"
+  },
+  {
+    "revision": "02d886b1578550dfaef6",
+    "url": "/audits/static/js/main.02d886b1.chunk.js"
+  },
+  {
+    "revision": "358e258069538d2d8f20",
+    "url": "/audits/static/css/1.b8a09fd3.chunk.css"
+  },
+  {
+    "revision": "358e258069538d2d8f20",
+    "url": "/audits/static/js/1.358e2580.chunk.js"
+  },
+  {
+    "revision": "8dbc246fddd19fa74126",
+    "url": "/audits/static/js/runtime~main.8dbc246f.js"
+  },
+  {
+    "revision": "d81ad7b439f9130bfde82f201ef00f09",
+    "url": "/audits/index.html"
+  }
+];
